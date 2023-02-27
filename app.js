c06bda58d5f783f7e128939fc1e4caf3cab3008b
gkon.cxo <gkon.cxo@gmail.com>
2023-02-27 11:49:38 +0900

    최초등록

---
+++ app.js
@@ -0,0 +1,14 @@
+
+<body>
+    <header>
+        <div class="back"></div>
+        <div class="head">
+            <h1>ux</h1>
+        </div>
+    </header>
+    <div class="box cf">
+        <div class="left">
+            <h3>소개<span class="line"></span></h3>
+        </div>
+    </div>
+</body>
